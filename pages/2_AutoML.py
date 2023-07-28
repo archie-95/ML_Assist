aa558f68e78cf2f9d9d9adea7e35317a6af73779
Archit Raj <archit.official0905@gmail.com>
2023-07-28 23:35:03 +0530

    AUTOML Start

--- pages/2_AutoML.py
+++ pages/2_AutoML.py
@@ -15,7 +15,18 @@ if 'df_aml' not in st.session_state:
     st.session_state.df_aml=None
 if 'uploaded_file_aml' not in st.session_state:
     st.session_state.uploaded_file_aml=None
+if 'target_var_aml' not in st.session_state:
+    st.session_state.target_var_aml=None
+if 'genre_aml' not in st.session_state:
+    st.session_state.genre_aml=None
 
+
+def make_grid(cols,rows):
+    grid = [0]*cols
+    for i in range(cols):
+        with st.container():
+            grid[i] = st.columns(rows)
+    return grid
 
 uploaded_file_aml = st.file_uploader("Choose a file")
 submit =st.button("Submit")
@@ -27,3 +38,27 @@ if submit:
 if st.session_state.df_aml is not None:
     st.write("File Uploaded is :",st.session_state.uploaded_file_aml.name)
     st.write(st.session_state.df_aml.head(5))
+
+    l_col,r_col=st.columns(2)
+    with l_col:
+        target_var_aml=st.selectbox ("Target",tuple(st.session_state.df_aml.columns))
+        genre_aml = st.selectbox("Type of Problem",('Classification', 'Regression'))
+
+    with r_col:
+        grid=make_grid(5,3)
+        # grid[0][0].write('00')
+        # grid[1][1].write('11')
+        # grid[2][2].write('22')
+        # grid[3][3].write('22')
+        with grid[4][1]:
+            begin_automl=st.button("Begin AutoML")
+    if begin_automl:
+        st.session_state.target_var_aml=target_var_aml
+        st.session_state.genre_aml=genre_aml
+    
+    if st.session_state.target_var_aml is not None and st.session_state.genre_aml is not None:
+        st.write("target Column Selected : ",st.session_state.target_var_aml)
+        st.write("Type of Problem : ",st.session_state.genre_aml)
+
+
+    
